0f176aaed3c5b8de820db83c082fc7fb20faf1e6
baekks93 <baekks93@gmail.com>
2026-01-08 09:21:15 +0900

    Java 시작 Hellow Java

--- HelloJava/Hello.java
+++ HelloJava/Hello.java
@@ -1,11 +1,10 @@
 package sec1;   // sec1는 패키지명
 
-/*
-Hello는 클래스 이름으로
-public 이며, public 클래스는 오로지 하나만 있어야하며,
-클래스 이름은 반드시 첫 글자는 대문자로 시작해야함
- */
-
+    /*
+        Hello는 클래스 이름으로
+        public 이며, public 클래스는 오로지 하나만 있어야하며,
+        클래스 이름은 반드시 첫 글자는 대문자로 시작해야함
+     */
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello~! Baek, JAVA"); // System.out.println의 단축 명령어는 sout
