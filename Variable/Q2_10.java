8ac3d80f0df5b268049cc748b46cc459ed0b4e18
baekks93 <baekks93@gmail.com>
2026-01-08 11:43:51 +0900

    2장 연습문제

---
+++ Variable/Q2_10.java
@@ -0,0 +1,16 @@
+package sec1;
+
+/*
+    52쪽에서 실습한 ‘문자형 연습’ 예제에서 ch1 = ‘Z’, ch2 = 38, ch3 = 97로 바꾸어 대입하면 출력 결과가 어떻게 될까요?
+ */
+public class Q2_10 {
+    public static void main(String[] args) {
+        char ch1 = 'Z';
+        char ch2 = 38;
+        char ch3 = 97;
+
+        System.out.println("ch1: " + ch1);
+        System.out.println("ch2: " + ch2);
+        System.out.println("ch3: " + ch3);
+    }
+}
