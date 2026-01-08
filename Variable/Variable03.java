1aee73001ee74b47b757c36d7d9c41cf08fc69e1
baekks93 <baekks93@gmail.com>
2026-01-08 09:20:32 +0900

    변수의 타입변환(형변환 - 자동/명시적)

--- Variable/Variable03.java
+++ Variable/Variable03.java
@@ -1,20 +1,20 @@
 package sec1;
 
+    /*
+        변수 명명 규칙
+        1. 영문 소문자로 시작하고, 뒤에 숫자를 포함할 수 있음 (단, 특수문자에서 $, _ 로 시작할 수 있음)
+        2. 예약어(keyword = Reserved word)는 사용할 수 없다. (단, 카멜케이스나 스네이크 방식을 활용하여 예약어를 변형할 수 있음)
+            ex) print_ln, printLn, break1, Break
+        3. 영문 대소문자를 구분한다.
+        4. 중간에 띄워쓰기 할 수 없음
+
+        관례
+        카멜케이스 방식을 활용하여 여러 단어를 연결하여 명명할 수 있다. (youAreStudent)
+        누구나 쉽게 해당 데이터가 무엇인지 알 수 있게 해야한다.
+    */
 public class Variable03 {
     public static void main(String[] args) {
 
-        /*
-            변수 명명 규칙
-            1. 영문 소문자로 시작하고, 뒤에 숫자를 포함할 수 있음 (단, 특수문자에서 $, _ 로 시작할 수 있음)
-            2. 예약어(keyword = Reserved word)는 사용할 수 없다. (단, 카멜케이스나 스네이크 방식을 활용하여 예약어를 변형할 수 있음)
-                ex) print_ln, printLn, break1, Break
-            3. 영문 대소문자를 구분한다.
-            4. 중간에 띄워쓰기 할 수 없음
-
-            관례
-            카멜케이스 방식을 활용하여 여러 단어를 연결하여 명명할 수 있다. (youAreStudent)
-            누구나 쉽게 해당 데이터가 무엇인지 알 수 있게 해야한다.
-         */
         char myChar = 'A';
 
         System.out.println("myChar : " + myChar);            // myChar : A
