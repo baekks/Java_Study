3ed92896a3a703ce5fbeab1acc9fa4b3838debfd
baekks93 <baekks93@gmail.com>
2026-01-08 11:52:51 +0900

    2장 연습문제

---
+++ Variable/Q2_11.java
@@ -0,0 +1,11 @@
+package sec1;
+
+/*
+    MY_AGE 상수를 선언하고 출력하도록 코드를 완성해 보세요.
+ */
+public class Q2_11 {
+    public static void main(String[] args) {
+        final int MY_AGE = 22;
+        System.out.println("MY_AGE: " + MY_AGE);
+    }
+}
---
+++ Variable/Q2_12.java
@@ -0,0 +1,14 @@
+package sec1;
+
+/*
+    52쪽에서 실습한 ‘문자형 연습’ 예제에서 형 변환을 사용했던 것을 기억하고 있나요? 주석 내용에 맞게 코드를 완성해 보세요
+ */
+public class Q2_12 {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        System.out.println((int)ch1);   // 문자에 해당하는 정숫값 출력
+
+        int ch2 = 67;
+        System.out.println((char)ch2);           // 정숫값에 해당하는 문자 출력
+    }
+}
