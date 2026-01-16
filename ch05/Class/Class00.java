663aa58e860c8e1271d13a6762ef134479c1d6cf
baekks93 <baekks93@gmail.com>
2026-01-16 09:12:02 +0900

    1/14 클래스 설명

---
+++ ch05/Class/Class00.java
@@ -0,0 +1,24 @@
+package sec1;
+
+public class Class00 {
+    public static void main(String[] args) {
+        //만약, 5명의 성적처리
+        int kor1 = 90;
+        int eng1 = 70;
+        int mat1 = 100;
+
+        System.out.println("1번 총점: "+(kor1+eng1+mat1));
+
+        int kor2 = 60;
+        int eng2 = 90;
+        int mat2 = 80;
+
+        System.out.println("2번 총점: "+(kor2+eng2+mat2));
+
+        int kor3 = 70;
+        int eng3 = 100;
+        int mat3 = 90;
+
+        System.out.println("3번 총점: "+(kor3+eng3+mat3));
+    }
+}
